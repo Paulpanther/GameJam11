3a7facf1ae8a29ffddbc569591c75fa204925603
Paul Methfessel <paul.methfessel@web.de>
2021-03-27 18:17:21 +0100

    Create Interactable HUD

--- Scripts/scene_stack.gd
+++ Scripts/scene_stack.gd
@@ -6,32 +6,29 @@ var root
 
 func _ready():
     root = get_tree().get_root()
-    scene_stack.append(_get_current_scene())
 
 func switch_scene(to_scene):
 	call_deferred("_switch_scene_deferred", to_scene)
 
 func _switch_scene_deferred(to_scene):
 	var level = _get_current_scene()
+	scene_stack.append(level)
 	level.hide()
 
 	# Add the next level
 	var next_level_resource = load(to_scene)
 	var next_level = next_level_resource.instance()
-	scene_stack.append(next_level)
-	print(scene_stack.size())
+	
 	root.add_child(next_level)
 
 func exit_scene():
 	call_deferred("_exit_scene_deferred")
 
 func _exit_scene_deferred():
-	print(scene_stack.size())
 	var current = _get_current_scene()
 	current.free()
-	scene_stack.pop_back()
 	
-	var next = scene_stack[scene_stack.size() - 1]
+	var next = scene_stack.pop_back()
 	next.show()
 	root.add_child(next)
 
