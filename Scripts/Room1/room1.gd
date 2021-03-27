5a71c3b68a948ffddcce2c81b9be36e231b2edac
Paul Methfessel <paul.methfessel@web.de>
2021-03-27 22:15:45 +0100

    Refactored directory structure

--- Scripts/Room1/room1.gd
+++ Scripts/Room1/room1.gd
@@ -2,7 +2,7 @@ extends Node
 
 func _on_GFCTop_input_event(viewport, event, shape_idx):
 	if (event is InputEventMouseButton && event.pressed):
-		SceneStack.switch_scene("res://Scenes/GrandfatherClockTop.tscn")
+		SceneStack.switch_scene("res://Scenes/Room1/GrandfatherClockTop.tscn")
 
 func on_detail_return():
 	get_node("GrandfatherClock").on_detail_return()
--- Scripts/RoomSideTemplate/template_room_side.gd
+++ Scripts/RoomSideTemplate/template_room_side.gd
@@ -3,4 +3,4 @@ extends Node
 
 func on_disk_clicked(viewport, event, shape_idx):
 	if (event is InputEventMouseButton && event.pressed):
-		SceneStack.switch_scene("res://Scenes/CatMouse.tscn")
+		SceneStack.switch_scene("res://Scenes/Room3/CatMouse.tscn")
