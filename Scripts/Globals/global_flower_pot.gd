d468d4eea8620051f57bc7be98078b3e0b1901cb
Louis Haub <louis.haub@googlemail.com>
2021-03-28 15:01:50 +0200

    fix minor issue with clock not working when rotated backwards

--- Scripts/Globals/global_flower_pot.gd
+++ Scripts/Globals/global_flower_pot.gd
@@ -2,6 +2,7 @@ extends Node
 
 var minutes = 260
 var minutes_win = 12 * 60 + 15
+var minutes_win_alternate = 15
 
 func get_rotation_hour_deg():
 	return (fmod(minutes, 720.0) / 720.0) * 360.0
@@ -11,7 +12,7 @@ func get_rotation_minute_deg():
 	return (fmod(minutes, 60.0) / 60.0) * 360.0
 
 func is_minutes_correct():
-	return abs(minutes - minutes_win) < 3
+	return abs(minutes - minutes_win) < 3 || abs(minutes - minutes_win_alternate) < 3
 
 var window_state = "closed"
 
