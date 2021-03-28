505818e212c79c91a5e86f15cdd7e852fecc847e
Paul Methfessel <paul.methfessel@web.de>
2021-03-28 12:26:35 +0200

    Add flower and table

--- Scripts/Room3/table_and_flower.gd
+++ Scripts/Room3/table_and_flower.gd
@@ -4,6 +4,25 @@ extends Node
 export (bool) var interactable = false
 
 
+func _ready():
+	_update_image()
+
+func on_detail_return():
+	_update_image()
+
+func on_flower_click(viewport, event, shape_idx):
+	if (event is InputEventMouseButton && event.pressed):
+		if interactable and GlobalFlowerPot.flower_state == 2:
+			GlobalFlowerPot.flower_state = 3
+			#Inv.inventory.add_item(Items.door_key)
+
+func on_table_click(viewport, event, shape_idx):
+	if (event is InputEventMouseButton && event.pressed):
+		if not interactable:
+			if GlobalFlowerPot.flower_state == 3:
+				SceneStack.switch_scene("res://Scenes/Room3/CatMouse.tscn")
+			else:
+				SceneStack.switch_scene("res://Scenes/Room3/TableDetail.tscn")
 
 func _update_image():
 	$Flower1.hide()
