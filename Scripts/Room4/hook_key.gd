a292aba362bf612e0deee1bab3549809ef573ddf
Louis Haub <louis.haub@googlemail.com>
2021-03-28 13:52:45 +0200

    fix hook key reappearing

--- Scripts/Room4/hook_key.gd
+++ Scripts/Room4/hook_key.gd
@@ -4,4 +4,5 @@ func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
 		if get_rect().has_point(to_local(event.position)) && visible == true:
 			Inv.inventory.add_item(Items.hook_shaped_key)
+			SolvedPuzzleStates.hook_key_picked_up = true
 			visible = false
