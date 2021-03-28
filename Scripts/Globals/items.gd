89a6c0f18dc2c51a48ae08a57dc0b21a798f2c67
Louis Haub <louis.haub@googlemail.com>
2021-03-28 15:20:04 +0200

    added scissors as item and updated item descriptions

--- Scripts/Globals/items.gd
+++ Scripts/Globals/items.gd
@@ -2,9 +2,10 @@ extends Node
 
 var _proto_item = preload("res://Scripts/item.gd")
 
-var poem_paper = _proto_item.new("res://Sprites/Items/paper.png", "Folded Paper", "You found a folded piece of paper", "res://Sprites/Items/poem.png")
-var hook_shaped_key = _proto_item.new("res://Sprites/Items/hook_shaped_key.png", "Hook shaped Key", "A hook-shaped key fall from the wall", null)
-var seeds = _proto_item.new("res://Sprites/Items/seeds_small.png", "Seeds", "You open the drawer and find a pack of labeled seeds", "res://Sprites/Items/seeds.png")
-var can = _proto_item.new("res://Sprites/Items/can.png", "Watering can", "In the closet is a filled watering can", null)
-var potato = _proto_item.new("res://Sprites/Items/potato.png","The Potato", "A Potato. Looks like it would taste great with cooked RED HERING", null)
-var door_key = _proto_item.new("res://Sprites/Items/door_key.png", "Door key", "One of the leaves of the flower contains a key", null)
+var poem_paper = _proto_item.new("res://Sprites/Items/paper.png", "Folded Paper", "A folded piece of paper.", "res://Sprites/Items/poem.png")
+var hook_shaped_key = _proto_item.new("res://Sprites/Items/hook_shaped_key.png", "A Hook shaped Key", "A hook-shaped key, that fell out of the wall", null)
+var seeds = _proto_item.new("res://Sprites/Items/seeds_small.png", "Seeds", "A bag. The label says something about seeds.", "res://Sprites/Items/seeds.png")
+var can = _proto_item.new("res://Sprites/Items/can.png", "Watering can", "A watering can. It seems pretty full.", null)
+var potato = _proto_item.new("res://Sprites/Items/potato.png","The Potato", "A Potato. Looks like it would taste great with cooked RED HERING.", null)
+var door_key = _proto_item.new("res://Sprites/Items/door_key.png", "Door key", "A key. I found it on the Flower.", null)
+var scissors = _proto_item.new("res://Sprites/Items/scissors.png", "Pair of Scissors", "Scissors. They look functional.", null)
